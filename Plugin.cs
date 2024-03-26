1e364d564ff2ae7f4e5d4fed512dd239e5cdb42f
BastedEggsRYummy <baimej@rpi.edu>
2024-03-26 17:36:29 -0400

    Started moving starting cash mod over to repo

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,31 @@
-﻿using BepInEx;
-namespace Template;
+﻿﻿using System;
+using BepInEx;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+namespace startingCash;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
     private void Awake()
     {
         // Plugin load logic goes here!
         // This script acts like a unity object.
-        Logger.LogInfo($"Hello World!");
+        Logger.LogInfo($"moreCash Active");
+        harmony.PatchAll(typeof(moreCash));
+    }
+
+    [HarmonyPatch(typeof(TimeOfDay), "Awake")]
+    
+    class moreCash
+    {
+        private static void Postfix(ref TimeOfDay __instance)
+        {
+            int startingAmount = 1000;
+            __instance.quotaVariables.startingCredits = startingAmount;
+        }  
     }
 }
