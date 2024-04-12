f68d7e1445c726d527efc25a341de0fc373f55ab
BastedEggsRYummy <baimej@rpi.edu>
2024-04-12 16:52:10 -0400

    Added comments and changed class name

--- Plugin.cs
+++ Plugin.cs
@@ -14,17 +14,22 @@ public class Plugin : BaseUnityPlugin
     {
         // Plugin load logic goes here!
         // This script acts like a unity object.
-        Logger.LogInfo($"startingCash Active");
-        harmony.PatchAll(typeof(moreCash));
+        Logger.LogInfo($"Starting Cash Active");
+        harmony.PatchAll(typeof(startingCash));
     }
 
+    //This defines the startingCash class to run on the TimeOfDay object on the Awake function
     [HarmonyPatch(typeof(TimeOfDay), "Awake")]
     
-    class moreCash
+    class startingCash
     {
+        //This is a postfix - it runs AFTER the normal awake function for all TimeOfDay objects
+        //Something to note is that there is only one TimeOfDay object - it's not like an enemy where there's many
         private static void Postfix(ref TimeOfDay __instance)
         {
+            //Sets the starting amount of cash to 1000 - change the number here to change the number of starting credits 
             int startingAmount = 1000;
+            //References the singular time of day object and navigates to the startingCredits value
             __instance.quotaVariables.startingCredits = startingAmount;
         }  
     }
